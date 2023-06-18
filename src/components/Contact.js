0362c30293fa1c234bd24017e0806121eb62ab82
ledigrace <cannuladygrace21@gmail.com>
2023-06-19 00:34:27 +0800

    deploy

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,42 +5,30 @@ import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
 export const Contact = () => {
-  const formInitialDetails = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    message: ''
-  }
-  const [formDetails, setFormDetails] = useState(formInitialDetails);
-  const [buttonText, setButtonText] = useState('Send');
-  const [status, setStatus] = useState({});
+  const [buttonText, setButtonText] = useState("Submit");
 
-  const onFormUpdate = (category, value) => {
-      setFormDetails({
-        ...formDetails,
-        [category]: value
-      })
-  }
-
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(formDetails),
-    });
-    setButtonText("Send");
-    let result = await response.json();
-    setFormDetails(formInitialDetails);
-    if (result.code == 200) {
-      setStatus({ succes: true, message: 'Message sent successfully'});
-    } else {
-      setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
-    }
+
+    const formData = new FormData(e.target);
+
+    fetch('https://api.apispreadsheets.com/data/OKSgZsiUYEtcqGmh/', {
+      method: 'POST',
+      body: formData
+    })
+      .then(response => {
+        if (response.ok) {
+          alert("Form Data Submitted :)");
+        } else {
+          alert("There was an error :(");
+        }
+        setButtonText("Submit");
+      })
+      .catch(error => {
+        alert("There was an error :(");
+        setButtonText("Submit");
+      });
   };
 
   return (
@@ -50,7 +38,7 @@ export const Contact = () => {
           <Col size={12} md={6}>
             <TrackVisibility>
               {({ isVisible }) =>
-                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us"/>
+                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us" />
               }
             </TrackVisibility>
           </Col>
@@ -58,34 +46,28 @@ export const Contact = () => {
             <TrackVisibility>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <h2>Get In Touch</h2>
-                <form onSubmit={handleSubmit}>
-                  <Row>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
-                    </Col>
-                    <Col size={12} className="px-1">
-                      <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                      <button type="submit"><span>{buttonText}</span></button>
-                    </Col>
-                    {
-                      status.message &&
-                      <Col>
-                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                  <h2>Get In Touch</h2>
+                  <form id="myForm"  onSubmit={handleSubmit}>
+                    <Row>
+                      <Col size={12} sm={6} className="px-1">
+                        <input type="text" placeholder="First Name" name="first_name" />
                       </Col>
-                    }
-                  </Row>
-                </form>
-              </div>}
+                      <Col size={12} sm={6} className="px-1">
+                        <input type="text" placeholder="Last Name" name="last_name" />
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input type="email" placeholder="Email Address" name="email" />
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input type="tel" placeholder="Phone No." name="phone" />
+                      </Col>
+                      <Col size={12} className="px-1">
+                        <textarea rows="6" placeholder="Message" name="message"></textarea>
+                        <button className="submit" type="submit" disabled={buttonText === "Sending..."}>{buttonText}</button>
+                      </Col>
+                    </Row>
+                  </form>
+                </div>}
             </TrackVisibility>
           </Col>
         </Row>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,13 +12,13 @@ export const Footer = () => {
           {/* <MailchimpForm /> */}
           <Col size={12} sm={6}>
             {/* <img src={logo} alt="Logo" /> */}
-            <h2>Lady Grace</h2>
+            <h2 className="footer-logo">Lady Grace</h2>
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://profile.indeed.com/?hl=en_PH&co=PH&from=gnav-passport--passport-webapp&_ga=2.251963466.258279234.1686983915-280137961.1686983915"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://web.facebook.com/lalalalady21"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://github.com/ledigrace"><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
